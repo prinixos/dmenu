50cef96d1bce3b77a9a8f27a1a99d9468932d844
prinixos <prinixos@protonmail.com>
2021-01-14 13:48:49 +0530

    dmenu ricing with new theme

--- config.h
+++ config.h
@@ -1,25 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-     "Hack:pixelsize=16:antialias=true:autohint=true",
+     "Hack:pixelsize=18:antialias=true:autohint=true",
      "JoyPixels:pixelsize=8:antialias=true:autohint=true"
    };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cccccc", "#282a36" },
-	[SchemeSel] = { "#ffffff", "#924441" },
+	[SchemeNorm] = { "#cccccc", "#1F1F1F" },
+	[SchemeSel] = { "#ffffff", "#C62828" },
 	[SchemeOut] = { "#000000", "#00ffff" },
     [SchemeMid] = { "#d7d7d7", "#1f2026" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 10;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
 static unsigned int columns    = 4;
 
 /*
@@ -28,4 +28,4 @@ static unsigned int columns    = 4;
  */
 static const char worddelimiters[] = " ";
 /* For bordering */
-static const unsigned int border_width = 2;
+static const unsigned int border_width = 4;
